ae871e4bcb139eb4bcdd439166da7d013bb4886e
kyleJL314 <kyle9639@colorado.edu>
2022-04-07 16:29:33 -0600

    add basic file and fuction

---
+++ .ipynb_checkpoints/discriminator-checkpoint.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+# To generate GIFs
+
+
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers
+import time
+
+from IPython import display
+print(tf.__version__)
+def make_discriminator_model():
+    model = tf.keras.Sequential()
+    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[28, 28, 1]))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dropout(0.3))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1))
+
+    return model
+discriminator = make_discriminator_model()
